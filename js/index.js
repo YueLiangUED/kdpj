3b726c834a066fd5a44affca5f0f8881acbc6523
wangbiaozy <56565283@qq.com>
2017-06-30 11:00:48 +0800

    9

--- js/index.js
+++ js/index.js
@@ -32,37 +32,37 @@ $(function () {
             if(e.target == $lis[0]){
                 $mesT.text('极不满意');
                 if(e.target.parentNode.id == 'estimateOne'){
-                    $labelOne.html('<li>安装速度慢</li><li>服务态度恶劣</li><li>未按指定时间到达安装</li>');
+                    $labelOne.html('<li>安装较不及时</li><li>安装人员态度差</li><li>未按预约时间到达</li><li>未指导使用宽带并测速</li><li>未提供装维联系卡</li><li>未穿鞋套</li><li>装机布线不规范</li><li>未着工装、工牌</li>');
                 }else if(e.target.parentNode.id == 'estimateTwo'){
-                    $labelTwo.html('<li>网速速度差</li><li>信号差</li><li>网速下载速度很慢</li>');
+                    $labelTwo.html('<li>信号差</li><li>网速慢</li><li>价格贵</li><li>优惠活动少</li><li>售后处理差</li>');
                 }
             }else if(e.target == $lis[1]){
                 $mesT.text('不满意');
                 if(e.target.parentNode.id == 'estimateOne'){
-                    $labelOne.html('<li>安装速度慢</li><li>服务态度恶劣</li><li>未按指定时间到达安装</li>');
+                    $labelOne.html('<li>安装较不及时</li><li>安装人员态度差</li><li>未按预约时间到达</li><li>未指导使用宽带并测速</li><li>未提供装维联系卡</li><li>未穿鞋套</li><li>装机布线不规范</li><li>未着工装、工牌</li>');
                 }else if(e.target.parentNode.id == 'estimateTwo'){
-                    $labelTwo.html('<li>网速速度差</li><li>信号差</li><li>网速下载速度很慢</li>');
+                    $labelTwo.html('<li>信号差</li><li>网速慢</li><li>价格贵</li><li>优惠活动少</li><li>售后处理差</li>');
                 }
             }else if(e.target == $lis[2]){
                 $mesT.text('一般');
                 if(e.target.parentNode.id == 'estimateOne'){
-                    $labelOne.html('<li>安装速度慢</li><li>服务态度差</li><li>未按指定时间到达安装</li>');
+                    $labelOne.html('<li>安装较及时</li><li>安装人员态度一般</li><li>按预约时间到达</li><li>指导使用宽带并测速</li><li>提供装维联系卡</li><li>装机布线规范</li>');
                 }else if(e.target.parentNode.id == 'estimateTwo'){
-                    $labelTwo.html('<li>网速速度慢</li><li>信号弱</li><li>网速下载速度慢</li>');
+                    $labelTwo.html('<li>信号一般</li><li>网速较快</li><li>价格便宜</li><li>优惠活动好</li><li>售后处理到位</li>');
                 }
             }else if(e.target == $lis[3]){
                 $mesT.text('满意');
                 if(e.target.parentNode.id == 'estimateOne'){
-                    $labelOne.html('<li>安装速度较快</li><li>服务态度不好</li><li>未按指定时间到达安装</li>');
+                    $labelOne.html('<li>安装较及时</li><li>安装人员态度一般</li><li>按预约时间到达</li><li>指导使用宽带并测速</li><li>提供装维联系卡</li><li>装机布线规范</li>');
                 }else if(e.target.parentNode.id == 'estimateTwo'){
-                    $labelTwo.html('<li>网速速度慢</li><li>信号弱</li><li>网速下载速度慢</li>');
+                    $labelTwo.html('<li>信号较好</li><li>网速较快</li><li>价格便宜</li><li>优惠活动较好</li><li>售后处理到位</li>');
                 }
             }else if(e.target == $lis[4]){
                 $mesT.text('非常满意');
                 if(e.target.parentNode.id == 'estimateOne'){
-                    $labelOne.html('<li>安装速度快</li><li>随叫随到</li><li>态度友好</li><li>按指定时间到达安装</li>');
+                    $labelOne.html('<li>安装及时</li><li>安装人员态度友好</li><li>按预约时间到达</li><li>指导使用宽带并测速</li><li>提供装维联系卡</li><li>装机布线规范</li>');
                 }else if(e.target.parentNode.id == 'estimateTwo'){
-                    $labelTwo.html('<li>网速速度快</li><li>信号强</li><li>网速下载速度快</li>');
+                    $labelTwo.html('<li>信号十分稳定</li><li>网速快</li><li>价格十分便宜</li><li>优惠活动好</li><li>售后处理到位</li>');
                 }
             }
         });
